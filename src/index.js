b5df304465d5dd810c6ceeb94912982025ef822c
AndSak <andriysak80@gmail.com>
2022-02-07 19:05:18 +0200

    feature: add class Users

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
- import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/';
+// import App from './components/app/';
 
+class Users extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {years:33};
+    }
+    render() {
+        const { name, surname, link } = this.props;
+        return (
+            <React.Fragment>
+                <h1>My name is {name} , last name-{surname}, years ={this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </React.Fragment>
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-  
+const AllName = () => {
+    return (
+        <>
+            <Users name="Bill" surname="Gets" link="facebook.com" />
+            <Users name="Charry" surname="Smith" link="vk.com" />
+            <Users name="John" surname="Chi" link="telegram.com" />
+        </>
+    )
+}
+ReactDOM.render(<AllName />, document.getElementById('root'));
