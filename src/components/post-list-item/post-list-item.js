5be46d277524447ec7d929f688473b1ba279232f
AndSak <andriysak80@gmail.com>
2022-02-08 21:56:01 +0200

    feature: add turnLike()

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -2,60 +2,47 @@ import React, { Component } from "react";
 
 import './post-list-item.css'
 
-export default class PostListItem extends Component {
-    constructor(props){
-        super(props);
-        this.state = {star: false};
-        this.turnStar = this.turnStar.bind(this);
-    }
-    turnStar(){
-        this.setState( ({star}) => ({star: !star}))
-    }
-    render() {
-        const {label} = this.props;
-        const {star} = this.state;
+class PostListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { star: false, like: false };
+    this.turnStar = this.turnStar.bind(this);
+    this.turnLike = this.turnLike.bind(this);
+  }
 
-        let classAll = "app-list-item d-flex justify-content-between";
-        if (star) { classAll += " important" }
+  turnStar() {
+    this.setState(({ star }) => ({ star: !star }))
+  }
 
-        return (
-            <div className={classAll}>
-                <span className="app-list-item-label">
-                    {label}
-                </span>
-                <div className="d-flex justify-content-center align-items-center">
-                    <button type="button" className="btn-star btn-sm" onClick={this.turnStar}>
-                        <i className="fa fa-star"></i>
-                    </button>
-                    <button type="button" className="btn-trash btn-sm">
-                        <i className="fa fa-trash-o"></i>
-                    </button>
-                    <i className="fa fa-heart"></i>
-                </div>
-            </div>
-        )
-    }
+  turnLike() {
+    this.setState(({ like }) => ({ like: !like }))
+  }
+
+  render() {
+    const { label } = this.props;
+    const { star, like } = this.state;
+    let classAll = "app-list-item d-flex justify-content-between";
+
+    if (star) { classAll += " important" }
+    if (like) { classAll += " like" }
+
+    return (
+      <div className={classAll}>
+        <span className="app-list-item-label" onClick={this.turnLike}>
+          {label}
+        </span>
+        <div className="d-flex justify-content-center align-items-center">
+          <button type="button" className="btn-star btn-sm" onClick={this.turnStar}>
+            <i className="fa fa-star"></i>
+          </button>
+          <button type="button" className="btn-trash btn-sm">
+            <i className="fa fa-trash-o"></i>
+          </button>
+          <i className="fa fa-heart"></i>
+        </div>
+      </div>
+    )
+  }
 }
-// const PostListItem = ({ label, star = false }) => {
 
-//     let classAll = "app-list-item d-flex justify-content-between";
-//     if (star) { classAll += " important" }
-
-//     return (
-//         <div className={classAll}>
-//             <span className="app-list-item-label">
-//                 {label}
-//             </span>
-//             <div className="d-flex justify-content-center align-items-center">
-//                 <button type="button" className="btn-star btn-sm">
-//                     <i className="fa fa-star"></i>
-//                 </button>
-//                 <button type="button" className="btn-trash btn-sm">
-//                     <i className="fa fa-trash-o"></i>
-//                 </button>
-//                 <i className="fa fa-heart"></i>
-//             </div>
-//         </div>
-//     )
-// }
-// export default PostListItem;
+export default PostListItem;
